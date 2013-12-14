3922fc3d96f4fa7f23634b3ef9450d768e48f7ab
ccastelot <ccastelot60@gmail.com>
2013-12-14 08:45:57 -0500

    Initial commit

---
+++ Workspace/Workspace/WSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WSAppDelegate.h
+//  Workspace
+//
+//  Created by Chris Castelot on 12/14/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Workspace/Workspace/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Workspace
+//
+//  Created by Chris Castelot on 12/14/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WSAppDelegate class]));
+    }
+}
